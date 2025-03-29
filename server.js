224df20aec565d0dd76cd7aa45d646c729f8527a
Vlad Tsibizov <vladtsibizov@yandex.ru>
2025-03-29 05:52:41 +0300

    fix

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ const app = express()
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.listen(3000, () => {
-  console.log('http://localhost:3000')
+  console.log('http://localhost')
 })
