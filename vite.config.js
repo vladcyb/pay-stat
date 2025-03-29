00dbdfc04bd48130ffeea54d7d53553356d20f98
Vlad Tsibizov <vladtsibizov@yandex.ru>
2025-03-29 17:14:23 +0300

    try

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,18 @@ import path from 'path'
 
 /** @type {import('vite').UserConfig} */
 export default {
-  root: path.resolve(__dirname, 'public'),
+  root: 'public',
+  base: '/pay-stat/',
   build: {
     outDir: path.resolve(__dirname, 'dist'),
   },
   server: {
     port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+      },
+    },
   },
 }
